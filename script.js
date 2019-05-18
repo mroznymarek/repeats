61f25f069eb8016183cce490519361b5c7d67db8
Marek Mroz <marekmroz@wp.pl>
2019-05-18 13:00:31 +0200

    classes Shape Circle

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-function Shape(color) {
-    this.element = document.createElement('div')
-    this.color = color
+class Shape {
+    constructor(color) {
+        this.element = document.createElement('div')
+        this.color = color
+    }
+    append() {
+        document.body.appendChild(this.element)
+    }
 }
 
-Shape.prototype.append = function () {
-    document.body.appendChild(this.element)
+class Circle extends Shape {
+    constructor(color) {
+        super(color)
+
+        this.element.style.backgroundColor = color
+        this.element.style.width = '100px'
+        this.element.style.height = '100px'
+        this.element.style.borderRadius = '50%'
+    }
+   roundCorners() { }
 }
-
-function Circle(color) {
-    Shape.call(this, color)
-
-    this.element.style.backgroundColor = color
-    this.element.style.width = '100px'
-    this.element.style.height = '100px'
-    this.element.style.borderRadius = '50%'
-}
-
-Circle.prototype = Object.create(Shape.prototype)
-Circle.prototype.constructor = Circle
-
-Circle.prototype.roundCorners = function () {}
 
 const circle1 = new Circle('blue')
